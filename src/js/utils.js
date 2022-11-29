d00a23902cd9efc234a1dbec3a3c18bf5dca733f
hol16046 <hol16046@byui.edu>
2022-11-28 19:58:57 -0700

    File Structure

---
+++ src/js/utils.js
@@ -0,0 +1,51 @@
+function convertToText (res) {
+    if (res.ok) {
+      return res.text();
+    } else {
+      throw new Error('Bad Response');
+    }
+}
+
+
+//Get URL Parameter
+export function getParam(param) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(param);
+}
+
+export function renderListWithTemplate(template, parent, list, callback) {
+    list.forEach(item => {
+        const clone = template.content.cloneNode(true);
+        const templateWithData = callback(clone, item);
+        parent.appendChild(templateWithData);
+    })
+}
+
+export function renderWithTemplate(template, parent, data, callback) {
+    let clone = template.content.cloneNode(true);
+    if(callback) {
+        clone = callback(clone,data);
+    } try {
+        parent.appendChild(clone);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+export async function loadTemplate (path) {
+    const html = await fetch(path).then(convertToText);
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template;
+}
+
+export async function loadHeaderFooter () {
+    const header = await loadTemplate('../partials/header.html');
+    const footer = await loadTemplate('../partials/footer.html');
+    const headerElement = document.getElementById('main-header');
+    const footerElement = document.getElementById('main-footer');
+    renderWithTemplate(header, headerElement);
+    getCartCount();
+    renderWithTemplate(footer, footerElement);
+}
